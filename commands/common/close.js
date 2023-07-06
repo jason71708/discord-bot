6b56023091ef976af4769e68208dac3a850469b1
Jason Zhuang <jason71708@gmail.com>
2023-07-06 12:11:33 +0800

    feat: remove testing command

--- commands/common/close.js
+++ commands/common/close.js
@@ -14,43 +14,48 @@ module.exports = {
     .setName('關機')
     .setDescription('儲存遊戲進度並關閉伺服器～'),
   async execute(interaction) {
-    await interaction.reply('關機中...');
+    try {
+      await interaction.reply('關機中...');
 
-    const ssh = new SSH({
-      host: host,
-      user: user,
-      key: fs.readFileSync(pemfile)
-    });
+      const ssh = new SSH({
+        host: host,
+        user: user,
+        key: fs.readFileSync(pemfile)
+      });
 
-    const prom = new Promise((resolve, reject) => {
-      let ourout = "";
-      ssh
-        .exec('pzserver quit', {
-          out: (stdout) => {
-            ourout += stdout;
-          }
-        })
-        .exec('sudo halt', {
-          exit: () => {
-            // ourout += "\nSuccessfully Exited!";
-            resolve();
-          },
-          out: (stdout) => { }
-        })
-        .start({
-          fail: (e) => {
-            console.log(e);
-            reject(e);
-          }
-        });
-    });
+      const prom = new Promise((resolve, reject) => {
+        let ourout = "";
+        ssh
+          .exec('pzserver quit', {
+            out: (stdout) => {
+              ourout += stdout;
+            }
+          })
+          .exec('sudo halt', {
+            exit: () => {
+              // ourout += "\nSuccessfully Exited!";
+              resolve();
+            },
+            out: (stdout) => { }
+          })
+          .start({
+            fail: (e) => {
+              console.log(e);
+              reject(e);
+            }
+          });
+      });
 
-    await prom;
-    // const res = await prom;
+      const res = await prom;
 
-    await interaction.editReply('已關機');
-    // await interaction.deferReply();
-    // await wait(4000);
-    // await interaction.reply('已關機');
+      await interaction.editReply('已關機');
+      // await interaction.deferReply();
+      // await wait(4000);
+      // await interaction.reply('已關機');
+
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
